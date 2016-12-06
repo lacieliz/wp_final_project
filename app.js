7ed91e9905c1723fc108150a14d5fbb9f995795b
lacieliz <lacieliz@naver.com>
2016-12-06 11:43:23 +0900

    bower_components

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/bower_components',  express.static(path.join(__dirname, '/bower_components')));
 
 app.use('/', index);
 app.use('/users', users);
