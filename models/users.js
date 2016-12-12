7bc67fad7973f08d949fefab4a4b211275a23a29
lacieliz <lacieliz@naver.com>
2016-12-13 00:18:53 +0900

    user schema

---
+++ models/users.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var schema = new Schema({
+  name: {type: String, required: true, trim: true},
+  email: {type: String, required: true, index: true, unique: true, trim: true},
+  password: {type: String},
+  createdAt: {type: Date, default: Date.now}
+}, {
+  toJSON: { virtuals: true},
+  toObject: {virtuals: true}
+});
+
+var User = mongoose.model('User', schema);
+
+module.exports = User;
